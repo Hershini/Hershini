d4c6ffa318730d0f23aa36aef0a9434d20e6e8e4
Hershini <109853909+Hershini@users.noreply.github.com>
2022-12-31 23:00:20 +0530

    Add files via upload

---
+++ template.py
@@ -0,0 +1,29 @@
+
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+#variable
+username="Admin"
+password="admin123"
+url="https://opensource-demo.orangehrmlive.com"
+
+class TestTemplate(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+    
+    def login(self, driver):
+        driver.find_element(By.NAME, "txtUsername").send_keys(username)
+        time.sleep(0.5)
+        driver.find_element(By.NAME, "txtPassword").send_keys(password)
+        time.sleep(0.5)
+        driver.find_element(By.NAME,"Submit").click()
+        time.sleep(3)
+
+    def tearDown(self): 
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
